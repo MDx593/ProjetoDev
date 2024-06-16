9ace8cc9d122ce3474b8c3ef3481eae268ac432b
Bruno Calazans <bcalazasncarritiha@gmail.com>
2024-06-16 16:32:57 -0300

    Formulario pronto

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,33 +8,83 @@
 
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
+
 <body>
     <nav class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Painel de Controle</a>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Painel de Controle</a>
+            </div>
+            <div id="navbar" class="collapse navbar-collapse">
+                <ul id="menu-principal" class="nav navbar-nav">
+                    <li class="active"><a ref_sys="sobre" href="#">Editar Sobre</a></li>
+                    <li><a ref_sys="cadastrar_equipe" href="#">Cadastrar Equipe</a></li>
+                    <li><a ref_sys="lista_equipe" href="#">Listar Equipe</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="?sair"><span class="glyphicon glyphicon-log-in"></span> Loggout</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul id="menu-principal" class="nav navbar-nav">
-            <li class="active"><a ref_sys="sobre" href="#">Editar Sobre</a></li>
-            <li><a ref_sys="cadastrar_equipe" href="#">Cadastrar Equipe</a></li>
-            <li><a ref_sys="lista_equipe" href="#">Listar Equipe</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="?sair"><span class="glyphicon glyphicon-log-in"></span> Loggout</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>    
+    </nav>
 
-    
+    <section class="container p-3">
+        <form>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Email</label>
+                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Senha</label>
+                    <input type="password" class="form-control" id="inputPassword4" placeholder="Senha">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputAddress">Endereço</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos Bobos, nº 0">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Endereço 2</label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartamento, hotel, casa, etc.">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">Cidade</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputEstado">Estado</label>
+                    <select id="inputEstado" class="form-control">
+                        <option selected>Escolher...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputCEP">CEP</label>
+                    <input type="text" class="form-control" id="inputCEP">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                    <label class="form-check-label" for="gridCheck">
+                        Clique em mim
+                    </label>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+    </section>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
+
 </html>
